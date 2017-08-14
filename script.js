a7d8eaf6f41c81185744787cbca8f922fc31f5ab
Thomas Binte <tbinte@gmail.com>
2017-08-15 00:09:41 +0200

    corrected conversion for western and southern coordinates

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-var dm = "N51° 25.322";
-var dm = "E007° 00.165";
+var dm = "E51° 25.322";
+/* var dm = "E007° 00.165"; */
 
-var re3 = /((N\d{2}|E\d{3})(°|\s|°\s))(\d{2}(\.|\,)\d{3})/;
+var re3 = /(([NS]\d{2}|[EW]\d{3})(°|\s|°\s))(\d{2}(\.|\,)\d{3})/;
 
 convertDMtoDecimal(dm);
 
 function convertDMtoDecimal(comp) {
     var re1 = /\d{2}\.\d{3}/;
-    var re2 = /((N\d{2}|E\d{3})(°|\s|°\s))/;
-    var rawminutes, rawdegrees, minutes, degrees;
+    var re2 = /(([NS]\d{2}|[EW]\d{3})(°|\s|°\s))/;
+    var re4 = /^(W|S)/;
+    var rawminutes, rawdegrees, minutes, degrees, convertedCoordinates, checkWestSouth;
+
+    checkWestSouth = re4.test(comp);
 
     rawminutes = comp.match(re1);
     rawdegrees = comp.match(re2);
+
     minutes = convertMinutesToDecimal(rawminutes);
     degrees = convertDegreesToDecimal(rawdegrees);
-    console.log("Minuten: " + minutes);
-    console.log("Grad: " + degrees);
-    console.log(degrees + minutes);
-    return degrees + minutes;
+    convertedCoordinates = (degrees + minutes); 
+
+    if (checkWestSouth) {
+        convertedCoordinates = Math.abs(convertedCoordinates) * -1;
+    } 
+
+    console.log(convertedCoordinates);
+    return convertedCoordinates;
 }
 
 function convertDegreesToDecimal(degrees) {
