2942e9bb3af9db58cb27c29bcc076cb9f653a8e5
Thomas Binte <tbinte@gmail.com>
2017-08-14 23:33:07 +0200

    complete rewrite for making it a library

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 var dm = "N51° 25.322";
-var degrees;
+var dm = "E007° 00.165";
 
-var re3 = /(N\d{2}(°|\s|°\s))(\d{2}(\.|\,)\d{3})/;
+var re3 = /((N\d{2}|E\d{3})(°|\s|°\s))(\d{2}(\.|\,)\d{3})/;
 
-extractMinutes(dm);
+convertDMtoDecimal(dm);
 
-function extractMinutes(comp) {
+function convertDMtoDecimal(comp) {
     var re1 = /\d{2}\.\d{3}/;
-    var re2 = /(N\d{2}(°|\s|°\s))/;
-    var minutes;
+    var re2 = /((N\d{2}|E\d{3})(°|\s|°\s))/;
+    var rawminutes, rawdegrees, minutes, degrees;
 
-    minutes = comp.match(re1);
-    degrees = comp.match(re2);
-    convertToDecimal(minutes);
+    rawminutes = comp.match(re1);
+    rawdegrees = comp.match(re2);
+    minutes = convertMinutesToDecimal(rawminutes);
+    degrees = convertDegreesToDecimal(rawdegrees);
+    console.log("Minuten: " + minutes);
+    console.log("Grad: " + degrees);
+    console.log(degrees + minutes);
+    return degrees + minutes;
 }
 
-function convertToDecimal(minutes) {
-    minutes = parseFloat(minutes);
-    var convertedDecimals = minutes / 60;
-    finalize(convertedDecimals);
-}
-
-function finalize(converted) {
-    var extractDegrees = /\d{2}/;
+function convertDegreesToDecimal(degrees) {
+    var extractDegrees = /\d{2,3}/;
     degrees = degrees[0].match(extractDegrees);
     degrees = parseFloat(degrees);
-    converted = degrees + converted;
-    converted = converted.toFixed(5);
-    console.log(converted);
+    return degrees;
+}
+
+function convertMinutesToDecimal(minutes) {
+    minutes = parseFloat(minutes);
+    var convertedDecimals = minutes / 60;
+    return convertedDecimals;
 }
